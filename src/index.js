502db05f5c73c82a6e423d7e2763013b14329bae
Yuvraj Dhakate <ydhakate@calibo.com>
2024-03-22 15:03:53 +0530

    updated template for nested path

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './home';
-import Dashboard from './dashboard-page';
-import Listing from './listing-page';
+import Dashboard from './dashboard';
+import Listing from './listing';
 import PageNotFound from './page-not-found';
 
 const App = () => {
@@ -14,12 +14,15 @@ const App = () => {
           <nav style={{margin: '20px'}}>
               <Link to="/" style={{marginRight: '20px'}}>Home</Link>
               <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
-              <Link to="/listing">Listing</Link>
+              <Link to="/dashboard/listing">Listing</Link>
           </nav>
+
           <Routes>
+              <Route path="/dashboard/*">
+                    <Route index element={<Dashboard />} />
+                    <Route path='listing' element={<Listing />} />
+              </Route>
               <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/listing" element={<Listing />} />
               <Route path="*" element={<PageNotFound />} />
           </Routes>
       </div>
